66b72df2640e9b9c540d003ec86839e38ff81228
MagicalCodeMonkey <mwoodru@gmail.com>
2019-10-03 14:43:11 -0400

    See if this fixes a linting issue

--- .apps/lidarr/lidarr_install.sh
+++ .apps/lidarr/lidarr_install.sh
@@ -8,7 +8,7 @@ lidarr_install()
 
     if [[ ! -d "${APP_PATH}" ]]; then
         info "Installing or updating ${APPNAME}"
-        curl -s -L -O $( curl -s https://api.github.com/repos/Lidarr/Lidarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 ) > /dev/null 2>&1 || fatal "Failed retrieve ${APPNAME}."
+        $(curl -s -L -O $(curl -s https://api.github.com/repos/Lidarr/Lidarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4) ) > /dev/null 2>&1 || fatal "Failed retrieve ${APPNAME}."
         mkdir -p "${APP_PATH}"
         tar -xvzf Lidarr.master.*.linux.tar.gz -C "${APP_PATH%${APPNAME}*}" > /dev/null 2>&1 || fatal "Failed to unpack ${APPNAME}"
         rm Lidarr.master.*.linux.tar.gz || error "Failed to remove  ${APPNAME} zip file."
