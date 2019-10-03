a2664dbabfa0fe6154fb9cf8f0d861b741f9f1d8
MagicalCodeMonkey <mwoodru@gmail.com>
2019-10-03 14:30:09 -0400

    Update for linting

--- .apps/bazarr/bazarr_post_install.sh
+++ .apps/bazarr/bazarr_post_install.sh
@@ -4,5 +4,5 @@ IFS=$'\n\t'
 
 bazarr_post_install()
 {
-    sudo -H -u "${APP_USER}" bash -c 'cd '${APP_PATH}'; pip install -U -r requirements.txt'  > /dev/null 2>&1 || error "Unable to install requirements for ${APP_NAME}"
+    sudo -H -u "${APP_USER}" bash -c "cd '${APP_PATH}'; pip install -U -r requirements.txt"  > /dev/null 2>&1 || error "Unable to install requirements for ${APP_NAME}"
 }
