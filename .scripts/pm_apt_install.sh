78c6d8579238e25cb0f493102dffd960847f8457
MagicalCodeMonkey <mwoodru@gmail.com>
2019-10-16 22:48:22 -0400

    Updates to make pm_apt_install better

--- .scripts/pm_apt_install.sh
+++ .scripts/pm_apt_install.sh
@@ -26,12 +26,12 @@ pm_apt_install() {
                 SOURCE_EXISTS=$(grep -c "${SOURCE_REPO}" "${SOURCE_FILE}" || echo "0")
                 local SOURCE_LINE_COUNT
                 SOURCE_LINE_COUNT=$(wc -l "${SOURCE_FILE}" | awk '{print $1}' || echo "0")
-                if [[ ${SOURCE_EXISTS} = 1 || ${SOURCE_LINE_COUNT} -gt 1 ]]; then
+                if [[ ${SOURCE_EXISTS} == 1 || ${SOURCE_LINE_COUNT} -gt 1 ]]; then
                     info "Adding/updating sources for ${APPNAME}"
                     if [[ -f ${SOURCE_FILE} ]]; then
                         rm "${SOURCE_FILE}"
                     fi
-                    echo "${SOURCE_REPO}" | tee "${SOURCE_FILE}" > /dev/null
+                    echo "${SOURCE_REPO}" >> "${SOURCE_FILE}"
                     UPDATE_APT="true"
                 fi
             elif [[ ${SOURCE_REPO} == ppa:* ]]; then
@@ -71,9 +71,9 @@ pm_apt_install() {
             run_script 'package_manager_run' repos
         fi
 
-        PACKAGE_VERSION_INSTALLED=$(sudo apt-cache policy "${APP_PACKAGE}" | grep "Installed:" | awk '{gsub("Installed:", ""); gsub(" ", ""); print}')
-        PACKAGE_VERSION_CANDIDATE=$(sudo apt-cache policy "${APP_PACKAGE}" | grep "Candidate:" | awk '{gsub("Candidate:", ""); gsub(" ", ""); print}')
-        if [[ ${PACKAGE_VERSION_INSTALLED} == "(none)" || ${PACKAGE_VERSION_INSTALLED} != "${PACKAGE_VERSION_CANDIDATE}" ]]; then
+        PACKAGE_VERSION_INSTALLED=$(sudo apt-cache policy "${APP_PACKAGE}" | grep -Po "Installed: \K.*" || true)
+        PACKAGE_VERSION_CANDIDATE=$(sudo apt-cache policy "${APP_PACKAGE}" | grep -Po "Candidate: \K.*" || true)
+        if [[ ${PACKAGE_VERSION_INSTALLED} == "" || ${PACKAGE_VERSION_INSTALLED} != "${PACKAGE_VERSION_CANDIDATE}" ]]; then
             if [[ ${APPDEPENDENCYOF} == "" ]]; then
                 notice "Installing or updating ${APPNAME} via apt"
             else
