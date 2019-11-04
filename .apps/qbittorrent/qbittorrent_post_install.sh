6f6220d89dc87c11e089b2b1ae058282e701a1c1
MagicalCodeMonkey <mwoodru@gmail.com>
2019-11-03 22:05:31 -0500

    Fix qbittorrent install

--- .apps/qbittorrent/qbittorrent_post_install.sh
+++ .apps/qbittorrent/qbittorrent_post_install.sh
@@ -3,10 +3,12 @@ set -euo pipefail
 IFS=$'\n\t'
 
 qbittorrent_post_install() {
-    local CONFIG_PATH="${DETECTED_HOMEDIR}/.config/${APPNAME}"
-    local CONFIG_FILE="${CONFIG_PATH}/qBittorrent.conf"
-    if [[ ! -d ${CONFIG_PATH} ]]; then
-        mkdir -p "${CONFIG_PATH}"
+    if [[ ! -d ${APP_CONFDIR_PATH} ]]; then
+        mkdir -p "${APP_CONFDIR_PATH}"
+    fi
+
+    local CONFIG_FILE="${APP_CONFDIR_PATH}/qBittorrent.conf"
+    if [[ ! -f ${CONFIG_FILE} ]]; then
         touch "${CONFIG_FILE}"
         echo "[LegalNotice]" >> "${CONFIG_FILE}"
         echo "Accepted=true" >> "${CONFIG_FILE}"
@@ -14,11 +16,11 @@ qbittorrent_post_install() {
 
     if [[ ! -d "/home/qbittorrent/.config/" ]]; then
         mkdir -p "/home/qbittorrent/.config/"
-        ln -s "${CONFIG_PATH}" "/home/qbittorrent/.config/qBittorrent"
+        ln -s "${APP_CONFDIR_PATH}" "/home/qbittorrent/.config/qBittorrent"
     fi
 
     chmod -R 770 "/home/qbittorrent/.config/"
     chown -R "${APP_USER}":"${APP_USER}" "/home/qbittorrent/.config/"
-    chmod -R 770 "${CONFIG_PATH}"
-    chown -R "${APP_USER}":"${APP_USER}" "${CONFIG_PATH}"
+    chmod -R 770 "${APP_CONFDIR_PATH}"
+    chown -R "${APP_USER}":"${APP_USER}" "${APP_CONFDIR_PATH}"
 }
